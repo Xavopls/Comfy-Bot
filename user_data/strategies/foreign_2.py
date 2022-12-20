be7ffb7be705e6e17fb7bb9fb517942c9b2390e2
XavoPls <xavi.oliza@gmail.com>
2022-12-20 21:03:10 +0100

    Foreign2: Updated ready to dry run

--- user_data/strategies/foreign_2.py
+++ user_data/strategies/foreign_2.py
@@ -11,123 +11,95 @@ from functools import reduce
 from freqtrade.persistence import Trade
 from datetime import datetime, timedelta, timezone
 from freqtrade.exchange import timeframe_to_prev_date
-import math
 import talib.abstract as ta
+import math
+import pandas_ta as pta
 import logging
 from logging import FATAL
+import time
+import requests
+import threading
 
 logger = logging.getLogger(__name__)
 
 
-# Certified Banger from my man Stash:
-# https://github.com/stash86/MultiMA_TSL/blob/main/user_data/strategies/Matoa/Matoa.py
+# https://github.com/stash86/MultiMA_TSL/blob/main/user_data/strategies/Cenderawasih/Cenderawasih_3.py
 
 class Foreign2(IStrategy):
+
+    def version(self) -> str:
+        return "v3"
 
     INTERFACE_VERSION = 3
 
     # ROI table:
     minimal_roi = {
-        "0": 100
+        "0": 100.0
     }
 
     # Buy hyperspace params:
     buy_params = {
-        "base_nb_candles_buy_hma": 62,
-        "low_offset_hma": 0.88,
+        "base_nb_candles_buy_hma": 42,
+        "low_offset_hma": 0.899,
 
-        "base_nb_candles_buy_hma2": 118,
-        "low_offset_hma2": 0.86,
-
-        "base_nb_candles_buy_hma3": 46,
-        "low_offset_hma3": 0.883,
-
-        "buy_length_volatility": 95,
-        "buy_max_volatility": 1.19,
-
-        "buy_length_volatility2": 110,
-        "buy_max_volatility2": 1.55,
     }
 
     # Sell hyperspace params:
     sell_params = {
-        "base_nb_candles_sell_ema": 49,
-        "high_offset_ema": 1.07,
+        "base_nb_candles_sell_ema": 31,
+        "high_offset_ema": 1.0,
 
-        "base_nb_candles_sell_ema2": 36,
-        "high_offset_ema2": 0.95,
+        "base_nb_candles_sell_ema2": 87,
+        "high_offset_ema2": 0.84,
 
-        "base_nb_candles_sell_ema3": 104,
-        "high_offset_ema3": 0.94,
+        "base_nb_candles_sell_ema3": 98,
+        "high_offset_ema3": 0.969,
 
-        "base_nb_candles_sell_ema4": 5,
-        "high_offset_ema4": 1.01,
+        "base_nb_candles_sell_ema4": 91,
+        "high_offset_ema4": 1.113,
+
+        "base_nb_candles_sell_zema": 93,
+        "high_offset_zema": 1.089,
+
+        "base_nb_candles_sell_zema2": 57,
+        "high_offset_zema2": 0.879,
     }
-
-    # Protection hyperspace params:
-    protection_params = {
-        "cooldown_lookback": 1,  # value loaded from strategy
-    }
-
-    cooldown_lookback = IntParameter(1, 48, default=2, space="protection", optimize=False)
-
-    @property
-    def protections(self):
-        prot = []
-
-        prot.append({
-            "method": "CooldownPeriod",
-            "stop_duration_candles": self.cooldown_lookback.value
-        })
-        return prot
-
-    dummy = IntParameter(20, 70, default=61, space='buy', optimize=True)
 
     optimize_buy_hma = False
-    base_nb_candles_buy_hma = IntParameter(5, 150, default=6, space='buy', optimize=optimize_buy_hma)
-    low_offset_hma = DecimalParameter(0.7, 0.99, default=0.98, decimals=2, space='buy', optimize=optimize_buy_hma)
+    base_nb_candles_buy_hma = IntParameter(5, 100, default=6, space='buy', optimize=optimize_buy_hma)
+    low_offset_hma = DecimalParameter(0.6, 0.99, default=0.95, space='buy', optimize=optimize_buy_hma)
 
-    optimize_buy_hma2 = False
-    base_nb_candles_buy_hma2 = IntParameter(5, 150, default=6, space='buy', optimize=optimize_buy_hma2)
-    low_offset_hma2 = DecimalParameter(0.7, 0.99, default=0.95, decimals=2, space='buy', optimize=optimize_buy_hma2)
+    optimize_sell_zema = False
+    base_nb_candles_sell_zema = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_zema)
+    high_offset_zema = DecimalParameter(1, 1.2, default=1, space='sell', optimize=optimize_sell_zema)
 
-    optimize_buy_hma3 = False
-    base_nb_candles_buy_hma3 = IntParameter(5, 150, default=6, space='buy', optimize=optimize_buy_hma3)
-    low_offset_hma3 = DecimalParameter(0.7, 0.99, default=0.95, space='buy', optimize=optimize_buy_hma3)
-
-    optimize_buy_volatility = False
-    buy_length_volatility = IntParameter(10, 200, default=72, space='buy', optimize=optimize_buy_volatility)
-    buy_min_volatility = DecimalParameter(0, 0.5, default=0, decimals=2, space='buy', optimize=False)
-    buy_max_volatility = DecimalParameter(0.5, 2, default=1, decimals=2, space='buy', optimize=optimize_buy_volatility)
-
-    optimize_buy_volatility2 = False
-    buy_length_volatility2 = IntParameter(10, 200, default=72, space='buy', optimize=optimize_buy_volatility2)
-    buy_max_volatility2 = DecimalParameter(0.5, 2, default=1, decimals=2, space='buy',
-                                           optimize=optimize_buy_volatility2)
+    optimize_sell_zema2 = False
+    base_nb_candles_sell_zema2 = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_zema2)
+    high_offset_zema2 = DecimalParameter(0.6, 0.99, default=0.95, space='sell', optimize=optimize_sell_zema2)
 
     optimize_sell_ema = False
-    base_nb_candles_sell_ema = IntParameter(5, 150, default=6, space='sell', optimize=optimize_sell_ema)
-    high_offset_ema = DecimalParameter(1, 1.2, default=1.02, decimals=2, space='sell', optimize=optimize_sell_ema)
+    base_nb_candles_sell_ema = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_ema)
+    high_offset_ema = DecimalParameter(1, 1.2, default=1, space='sell', optimize=optimize_sell_ema)
 
     optimize_sell_ema2 = False
-    base_nb_candles_sell_ema2 = IntParameter(5, 150, default=6, space='sell', optimize=optimize_sell_ema2)
-    high_offset_ema2 = DecimalParameter(0.7, 0.99, default=0.98, decimals=2, space='sell', optimize=optimize_sell_ema2)
+    base_nb_candles_sell_ema2 = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_ema2)
+    high_offset_ema2 = DecimalParameter(0.7, 0.99, default=0.95, space='sell', optimize=optimize_sell_ema2)
 
     optimize_sell_ema3 = False
-    base_nb_candles_sell_ema3 = IntParameter(5, 150, default=6, space='sell', optimize=optimize_sell_ema3)
-    high_offset_ema3 = DecimalParameter(0.7, 0.99, default=0.95, decimals=2, space='sell', optimize=optimize_sell_ema3)
+    base_nb_candles_sell_ema3 = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_ema3)
+    high_offset_ema3 = DecimalParameter(0.7, 0.99, default=0.95, space='sell', optimize=optimize_sell_ema3)
 
     optimize_sell_ema4 = False
-    base_nb_candles_sell_ema4 = IntParameter(5, 150, default=6, space='sell', optimize=optimize_sell_ema4)
-    high_offset_ema4 = DecimalParameter(1, 1.2, default=1, decimals=2, space='sell', optimize=optimize_sell_ema4)
+    base_nb_candles_sell_ema4 = IntParameter(5, 100, default=6, space='sell', optimize=optimize_sell_ema4)
+    high_offset_ema4 = DecimalParameter(1, 1.2, default=1, space='sell', optimize=optimize_sell_ema4)
 
     # Stoploss:
-    stoploss = -0.08
+    stoploss = -0.1
 
     # Trailing stop:
-    trailing_stop = True
-    trailing_stop_positive = 0.01
-    trailing_stop_positive_offset = 0.08
+    trailing_stop = False
+    trailing_stop_positive = 0.001
+    trailing_stop_positive_offset = 0.01
     trailing_only_offset_is_reached = True
 
     # Sell signal
@@ -139,14 +111,14 @@ class Foreign2(IStrategy):
     timeframe = '5m'
 
     process_only_new_candles = True
-    startup_candle_count = 300
+    startup_candle_count = 200
 
     age_filter = 30
 
     @informative('1d')
     def populate_indicators_1d(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe['age_filter_ok'] = (
-                    dataframe['volume'].rolling(window=self.age_filter, min_periods=self.age_filter).min() > 0)
+                dataframe['volume'].rolling(window=self.age_filter, min_periods=self.age_filter).min() > 0)
 
         if not self.config['runmode'].value in ('dry_run', 'live'):
             drop_columns = ['open', 'high', 'low', 'close', 'volume']
@@ -162,23 +134,13 @@ class Foreign2(IStrategy):
             dataframe['hma_offset_buy'] = tv_hma(dataframe,
                                                  int(self.base_nb_candles_buy_hma.value)) * self.low_offset_hma.value
 
-        if not self.optimize_buy_hma2:
-            dataframe['hma_offset_buy2'] = tv_hma(dataframe,
-                                                  int(self.base_nb_candles_buy_hma2.value)) * self.low_offset_hma2.value
+        if not self.optimize_sell_zema:
+            dataframe['zema_offset_sell'] = zema(dataframe,
+                                                 int(self.base_nb_candles_sell_zema.value)) * self.high_offset_zema.value
 
-        if not self.optimize_buy_hma3:
-            dataframe['hma_offset_buy3'] = tv_hma(dataframe,
-                                                  int(self.base_nb_candles_buy_hma3.value)) * self.low_offset_hma3.value
-
-        if not self.optimize_buy_volatility:
-            df_std = dataframe['close'].rolling(int(self.buy_length_volatility.value)).std()
-            dataframe["volatility"] = (df_std > self.buy_min_volatility.value) & (
-                        df_std < self.buy_max_volatility.value)
-
-        if not self.optimize_buy_volatility2:
-            df_std = dataframe['close'].rolling(int(self.buy_length_volatility2.value)).std()
-            dataframe["volatility2"] = (df_std > self.buy_min_volatility.value) & (
-                        df_std < self.buy_max_volatility2.value)
+        if not self.optimize_sell_zema2:
+            dataframe['zema_offset_sell2'] = zema(dataframe,
+                                                  int(self.base_nb_candles_sell_zema2.value)) * self.high_offset_zema2.value
 
         if not self.optimize_sell_ema:
             dataframe['ema_offset_sell'] = ta.EMA(dataframe,
@@ -206,55 +168,20 @@ class Foreign2(IStrategy):
             dataframe['hma_offset_buy'] = tv_hma(dataframe,
                                                  int(self.base_nb_candles_buy_hma.value)) * self.low_offset_hma.value
 
-        if self.optimize_buy_hma2:
-            dataframe['hma_offset_buy2'] = tv_hma(dataframe,
-                                                  int(self.base_nb_candles_buy_hma2.value)) * self.low_offset_hma2.value
-
-        if self.optimize_buy_hma3:
-            dataframe['hma_offset_buy3'] = tv_hma(dataframe,
-                                                  int(self.base_nb_candles_buy_hma3.value)) * self.low_offset_hma3.value
-
-        if self.optimize_buy_volatility:
-            df_std = dataframe['close'].rolling(int(self.buy_length_volatility.value)).std()
-            dataframe["volatility"] = (df_std > self.buy_min_volatility.value) & (
-                        df_std < self.buy_max_volatility.value)
-
-        if self.optimize_buy_volatility2:
-            df_std = dataframe['close'].rolling(int(self.buy_length_volatility2.value)).std()
-            dataframe["volatility2"] = (df_std > self.buy_min_volatility.value) & (
-                        df_std < self.buy_max_volatility2.value)
-
-        dataframe['enter_tag'] = ''
+        dataframe.loc[:, 'enter_tag'] = ''
+        dataframe.loc[:, 'enter_long'] = 0
 
         add_check = (
                 dataframe['live_data_ok']
                 &
                 dataframe['age_filter_ok_1d']
-                &
-                (dataframe['close'] < dataframe['open'])
         )
 
         buy_offset_hma = (
-                (dataframe['close'] < dataframe['hma_offset_buy'])
-                &
-                (dataframe['volatility'].shift() == True)
+            (dataframe['close'] < dataframe['hma_offset_buy'])
         )
         dataframe.loc[buy_offset_hma, 'enter_tag'] += 'hma '
         conditions.append(buy_offset_hma)
-
-        buy_offset_hma2 = (
-                ((dataframe['close'] < dataframe['hma_offset_buy2']).rolling(2).min() > 0)
-                &
-                (dataframe['volatility2'].shift() == True)
-        )
-        dataframe.loc[buy_offset_hma2, 'enter_tag'] += 'hma_2 '
-        conditions.append(buy_offset_hma2)
-
-        buy_offset_hma3 = (
-                (dataframe['close'] < dataframe['hma_offset_buy3']).rolling(3).min() > 0
-        )
-        dataframe.loc[buy_offset_hma3, 'enter_tag'] += 'hma_3 '
-        conditions.append(buy_offset_hma3)
 
         if conditions:
             dataframe.loc[
@@ -264,18 +191,17 @@ class Foreign2(IStrategy):
                 'enter_long',
             ] = 1
 
-            dataframe.loc[
-                buy_offset_hma
-                &
-                buy_offset_hma2
-                &
-                np.invert(buy_offset_hma3),
-                'enter_long'
-            ] = 0
-
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        if self.optimize_sell_zema:
+            dataframe['zema_offset_sell'] = zema(dataframe,
+                                                 int(self.base_nb_candles_sell_zema.value)) * self.high_offset_zema.value
+
+        if self.optimize_sell_zema2:
+            dataframe['zema_offset_sell2'] = zema(dataframe,
+                                                  int(self.base_nb_candles_sell_zema2.value)) * self.high_offset_zema2.value
 
         if self.optimize_sell_ema:
             dataframe['ema_offset_sell'] = ta.EMA(dataframe,
@@ -296,29 +222,41 @@ class Foreign2(IStrategy):
         dataframe.loc[:, 'exit_tag'] = ''
         conditions = []
 
+        sell_zema_1 = (
+            (dataframe['close'] > dataframe['zema_offset_sell'])
+        )
+        conditions.append(sell_zema_1)
+        dataframe.loc[sell_zema_1, 'exit_tag'] += 'ZEMA_1 '
+
+        sell_zema_2 = (
+            (dataframe['close'] < dataframe['zema_offset_sell2'])
+        )
+        conditions.append(sell_zema_2)
+        dataframe.loc[sell_zema_2, 'exit_tag'] += 'ZEMA_2 '
+
         sell_ema_1 = (
             (dataframe['close'] > dataframe['ema_offset_sell'])
         )
         conditions.append(sell_ema_1)
-        dataframe.loc[sell_ema_1, 'exit_tag'] += 'EMA_up '
+        dataframe.loc[sell_ema_1, 'exit_tag'] += 'EMA_1 '
 
         sell_ema_2 = (
-            (dataframe['close'] < dataframe['ema_offset_sell2'])
+                (dataframe['close'] < dataframe['ema_offset_sell2']).rolling(2).min() > 0
         )
         conditions.append(sell_ema_2)
-        dataframe.loc[sell_ema_2, 'exit_tag'] += 'EMA_down '
+        dataframe.loc[sell_ema_2, 'exit_tag'] += 'EMA_2 '
 
         sell_ema_3 = (
-                (dataframe['close'] < dataframe['ema_offset_sell3']).rolling(2).min() > 0
+            (dataframe['close'] < dataframe['ema_offset_sell3'])
         )
         conditions.append(sell_ema_3)
-        dataframe.loc[sell_ema_3, 'exit_tag'] += 'EMA_down_2 '
+        dataframe.loc[sell_ema_3, 'exit_tag'] += 'EMA_3 '
 
         sell_ema_4 = (
-                (dataframe['close'] > dataframe['ema_offset_sell4']).rolling(3).min() > 0
+                (dataframe['close'] > dataframe['ema_offset_sell4']).rolling(2).min() > 0
         )
         conditions.append(sell_ema_4)
-        dataframe.loc[sell_ema_4, 'exit_tag'] += 'EMA_up_3 '
+        dataframe.loc[sell_ema_4, 'exit_tag'] += 'EMA_4 '
 
         add_check = (
             (dataframe['volume'] > 0)
@@ -375,3 +313,18 @@ def tv_hma(dataframe, length=9, field='close') -> DataFrame:
     # dataframe.drop("h", inplace=True, axis=1)
 
     return tv_hma
+
+
+def zema(dataframe, period, field='close'):
+    """
+    Source: https://github.com/freqtrade/technical/blob/master/technical/indicators/overlap_studies.py#L79
+    Modified slightly to use ta.EMA instead of technical ema
+    """
+    df = dataframe.copy()
+
+    df['ema1'] = ta.EMA(df[field], timeperiod=period)
+    df['ema2'] = ta.EMA(df['ema1'], timeperiod=period)
+    df['d'] = df['ema1'] - df['ema2']
+    df['zema'] = df['ema1'] + df['d']
+
+    return df['zema']
